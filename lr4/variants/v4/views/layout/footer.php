01e0a86cd169fbd9566dd19e31aaf851a6c7344a
Nate <kn242_gma@student.ztu.edu.ua>
2026-03-22 14:41:53 +0200

    swiched v30 to v4

--- lr4/variants/v4/views/layout/footer.php
+++ lr4/variants/v4/views/layout/footer.php
@@ -2,7 +2,7 @@
     </main>
     <footer class="footer">
         <div class="container">
-            <p>&copy; <?= date('Y') ?> Кулінарний блог &mdash; Варіант 30, Лабораторна робота №4-5</p>
+            <p>&copy; <?= date('Y') ?> Піцерія &mdash; Варіант 4, Лабораторна робота №4-5</p>
         </div>
     </footer>
 </body>
